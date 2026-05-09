8c6c3a721df03d7b7f1d32a70ee78a1c7e0fe975
Cyril <gonecd@gmail.com>
2026-05-09 12:29:26 +0200

    Update of May 2026

---
+++ iPhone Seriez/Sources/CommonSensMedia.swift
@@ -0,0 +1,9 @@
+//
+//  CommonSensMedia.swift
+//  SerieA
+//
+//  Created by Cyril DELAMARE on 03/05/2026.
+//  Copyright © 2026 Home. All rights reserved.
+//
+
+import Foundation
---
+++ iPhone Seriez/Sources/FreeBox.swift
@@ -0,0 +1,9 @@
+//
+//  FreeBox.swift
+//  SerieA
+//
+//  Created by Cyril DELAMARE on 11/11/2025.
+//  Copyright © 2025 Home. All rights reserved.
+//
+
+import Foundation
